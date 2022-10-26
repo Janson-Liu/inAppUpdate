736303c9f99d29659d6c2b280becda5e21dd0f15
Janson <philialjj@hotmail.com>
2022-10-26 16:38:12 +1030

    new module

--- app/src/main/java/com/example/inappupdate/MainActivity.kt
+++ app/src/main/java/com/example/inappupdate/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.inappupdate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+import com.example.upgrade.UpgradeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.test).setOnClickListener {
+            startActivity(Intent(this,UpgradeActivity::class.java))
+        }
     }
 }
